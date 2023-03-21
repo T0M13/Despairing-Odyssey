ca1d32cebf2a2cb4bfff9699421c12f1fdc42dcf
tomi <herceg133@gmail.com>
2023-03-21 10:01:48 +0100

    Refactored SaveCheckpoint Logic

--- DesparingOdyssey/Assets/SaveCheckpoint.cs
+++ DesparingOdyssey/Assets/SaveCheckpoint.cs
@@ -7,17 +7,37 @@ public class SaveCheckpoint : MonoBehaviour
 {
     [SerializeField] private Transform spawnPoint;
 
-    private void OnTriggerEnter(Collider other)
+
+
+    [SerializeField] private float saveTime = 3;
+    [SerializeField] private float saveTimer;
+
+
+    void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<PlayerController>())
         {
-            PlayerController player = other.GetComponent<PlayerController>();
-            if (player.inventoryBehaviour.inventoryItemSlots.Contains(InventoryItemType.SaveItem) && !player.SavedPositionSaved)
+            saveTimer = saveTime;
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+            saveTimer -= Time.deltaTime;
+            if (saveTimer <= 0)
             {
-                player.SavedPosition = new Vector3(transform.position.x, spawnPoint.position.y, transform.position.z);
-                player.SavedPositionSaved = true;
-                player.inventoryBehaviour.RemoveItem(InventoryItemType.SaveItem, 1);
-                player.UpdateInventory();
+                saveTimer = saveTime;
+
+                PlayerController player = other.GetComponent<PlayerController>();
+                if (player.inventoryBehaviour.inventoryItemSlots.Contains(InventoryItemType.SaveItem) && !player.SavedPositionSaved)
+                {
+                    player.SavedPosition = new Vector3(transform.position.x, spawnPoint.position.y, transform.position.z);
+                    player.SavedPositionSaved = true;
+                    player.inventoryBehaviour.RemoveItem(InventoryItemType.SaveItem, 1);
+                    player.UpdateInventory();
+                }
             }
         }
     }
