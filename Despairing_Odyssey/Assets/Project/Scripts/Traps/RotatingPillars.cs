b2c0f287369eeb78e1639e63aca325ca14eec8e8
Krieger <levi.tiziani@gmail.com>
2023-03-24 02:22:44 +0100

    Added Rotating Pillars

---
+++ Despairing_Odyssey/Assets/Project/Scripts/Traps/RotatingPillars.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingPillars : MonoBehaviour
+{
+    public float speed = 5f;
+    public Vector3 axis = Vector3.up;
+    public bool rotateClockwise = true;
+
+    void Update()
+    {
+        float direction = rotateClockwise ? 1f : -1f;
+
+        transform.Rotate(axis, direction * speed * Time.deltaTime);
+    }
+}
