199ef84cb04a3355dac711eef7b4e69780590cd6
Krieger <levi.tiziani@gmail.com>
2023-03-28 06:19:38 +0200

    Added Bounce Pad (with Spikes)

--- Despairing_Odyssey/Assets/Project/Scripts/Traps/BouncePad.cs
+++ Despairing_Odyssey/Assets/Project/Scripts/Traps/BouncePad.cs
@@ -8,6 +8,20 @@ public class BouncePad : MonoBehaviour
     public float bounceThreshold = -5f;
     public Vector3 bounceDirection = Vector3.up;
 
+    //[Range(0, 6)]
+    //[SerializeField] private int spikeAmount = 0;
+    //[SerializeField] private GameObject[] spikes = new GameObject[6];
+
+    //private void Start()
+    //{
+    //    SetSwordAmount();
+    //}
+
+    //private void OnValidate()
+    //{
+    //    SetSwordAmount();
+    //}
+   
     private void OnTriggerEnter(Collider col)
     {
         if (col.GetComponent<PlayerController>() && col.GetComponent<Rigidbody>().velocity.y <= bounceThreshold)
@@ -23,4 +37,17 @@ public class BouncePad : MonoBehaviour
             }
         }
     }
+
+    //private void SetSwordAmount()
+    //{
+    //    for (int i = 0; i < spikes.Length; i++)
+    //    {
+    //        spikes[i].SetActive(false);
+    //    }
+
+    //    for (int i = 0; i < spikeAmount; i++)
+    //    {
+    //        spikes[i].SetActive(true);
+    //    }
+    //}
 }
